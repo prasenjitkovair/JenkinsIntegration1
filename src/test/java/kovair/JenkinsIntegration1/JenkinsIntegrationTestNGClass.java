ec1bc3355a5f681c8a658c0279c21dcaabd29753
prasenjitkovair <prasenjitok@gmail.com>
2019-06-11 15:43:40 +0530

    report commit 3

--- src/test/java/kovair/JenkinsIntegration1/JenkinsIntegrationTestNGClass.java
+++ src/test/java/kovair/JenkinsIntegration1/JenkinsIntegrationTestNGClass.java
@@ -37,8 +37,8 @@ public class JenkinsIntegrationTestNGClass
 	  @Test
 	  public void NGTest1() {
 		  System.out.println("In Test 1");
-		  //Assert.assertTrue(true);
-		  Assert.fail();
+		  Assert.assertTrue(true);
+		  //Assert.fail();
 		  //takeSnapShot(driver);
 	  }
   
@@ -47,8 +47,8 @@ public class JenkinsIntegrationTestNGClass
 	  public void NGTest2() {
 		  System.out.println("In Test 2");
 		  //Assert.fail();
-		  //Assert.assertTrue(true);
-		  throw new SkipException("NGTest2 skipped");
+		  Assert.assertTrue(true);
+		  //throw new SkipException("NGTest2 skipped");
 		  
 	  }
 	  
